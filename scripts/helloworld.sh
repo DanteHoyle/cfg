128befa4a1f8c9129f874c1651ef9868e13209b6
Dante Hoyle <hoyledante@gmail.com>
2024-02-04 20:19:19 -0800

    Added an example hello world bash script

---
+++ scripts/helloworld.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Sets variable hello to string hello
+hello="Hello"
+world='world'
+punctuation_comma=,
+punctuation_exclamation=!
+
+# this disgusting run of variables arrangest he string "Hello, world!"
+echo $hello$punctuation_comma $world$punctuation_exclamation
+
