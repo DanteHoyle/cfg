c94579256f496c039db96713fe6bfd53aa713108
Dante Hoyle <hoyledante@gmail.com>
2024-02-10 13:25:07 -0800

    sync_source_to_live.fish works for vim

--- scripts/sync_source_to_live.fish
+++ scripts/sync_source_to_live.fish
@@ -4,6 +4,20 @@
 # this script pulls config files from the running config and stores it in the dotfiles directory
 # use at your own risk - only tested on a fedora 39 workstation
 
-function sync_vim
+set VIM_PATH ~/.vim
 
+
+function sync_vim
+    echo Syncing vimrc to $VIM_PATH
+    if cp vimrc $VIM_PATH
+        echo Success
+    else
+        echo Error! exiting script...\n
+    end
 end
+
+function sync_all
+    sync_vim
+end
+
+sync_all
