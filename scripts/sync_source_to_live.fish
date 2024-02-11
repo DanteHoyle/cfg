eec731d402b48987a91c3f0129648a23daee94dd
Dante Hoyle <hoyledante@gmail.com>
2024-02-11 14:03:50 -0800

    More work on functions for syncing configs - stil incomplete

--- scripts/sync_source_to_live.fish
+++ scripts/sync_source_to_live.fish
@@ -5,19 +5,37 @@
 # use at your own risk - only tested on a fedora 39 workstation
 
 set VIM_PATH ~/.vim
-
+set FISH_PATH __fish_confi_dir
+set TMUX_PATH $HOME
 
 function sync_vim
-    echo Syncing vimrc to $VIM_PATH
+    echo Syncing vimrc to $VIM_PATH/vimrc
     if cp vimrc $VIM_PATH
         echo Success
     else
         echo Error! exiting script...\n
+        exit 1
     end
+end
+
+function sync_fish
+    echo Syncing config.fish to $FISH_PATH
+    if cp config.fish $FISH_PATH
+        echo Success
+    else
+        echo Error! exiting script... \n
+        exit 1
+    end
+
+end
+
+function sync_tmux
+    echo Syncing tmux.conf to $TMUX_PATH as .tmux.conf
+
 end
 
 function sync_all
     sync_vim
 end
 
-sync_all
+sync_tmux
