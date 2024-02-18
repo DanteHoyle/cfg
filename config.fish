ca831dff7ce9a45a524fff4a7e250023137d5085
Dante Hoyle <hoyledante@gmail.com>
2024-02-17 19:37:45 -0800

    removed unused aliases, and functions from config.fish

--- config.fish
+++ config.fish
@@ -1,9 +1,5 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    abbr --add cfgfish vim ~/.config/fish/config.fish
-    abbr --add cfgvim vim ~/.vim/vimrc
-    abbr --add cfgtmux vim ~/.tmux.conf
-
+    # joins existing tmux session if one exists, creates a new one otherwise
     if tmux has-session
         tmux attach
     else
@@ -13,9 +9,4 @@ end
 
 function reload_fish
     source ~/.config/fish/config.fish
-end
-
-function update_system
-    sudo apt update
-    sudo apt upgrade
 end
