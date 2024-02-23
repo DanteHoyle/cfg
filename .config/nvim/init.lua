abec0ffda151d65b1270f8aa273a9e59066c8f1d
Dante Hoyle <hoyledante@gmail.com>
2024-02-23 09:38:54 -0800

    updated init.lua with actual settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,14 @@
-require("config.config")
+--- General Config
+
+-- Sets up relative line numbering with absolute position on current line
+vim.opt.relativenumber = true
+vim.opt.nu = true
+
+-- Enables Mouse Support
+vim.opt.mouse = 'a'
+
+-- Tab Settings
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
