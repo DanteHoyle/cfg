f841dcef7839df17b48da9cc9505ffce446304c8
Dante Hoyle <hoyledante@gmail.com>
2024-02-22 19:11:59 -0800

    first test of bare git

--- .config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-	# Aliases
-	alias vim nvim
-	# Used for tracking with bare git repo 
-	alias config='/usr/bin/git --git-dir=$HOME/Repos/dotfiles/ --work-tree=$HOME'
-
-	# Environment Variables
-	set -Ux EDITOR /usr/bin/nvim
-end
--- .config/fish/functions/fish_greeting.fish
+++
@@ -1,4 +0,0 @@
-# controls startup message when fish starts
-function fish_greeting
-    # intentionally empty so there is no popup when fish starts
-end
