0d84ab027ea10389ad94e265b797b88fbefdf555
Dante Hoyle <hoyledante@gmail.com>
2024-02-23 20:36:17 -0800

    moved assumed path from /home/dante/Repos/dotfiles to /home/dante/.cfg

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
 	# Aliases
 	alias vim nvim
 	# Used for tracking with bare git repo 
-	alias config='/usr/bin/git --git-dir=$HOME/Repos/dotfiles/ --work-tree=$HOME'
+	alias config='/usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME'
 
 	# Environment Variables
 	set -gx EDITOR /usr/bin/nvim
