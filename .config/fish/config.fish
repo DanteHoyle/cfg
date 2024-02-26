81e3b22065a89fe605d475a53360d66ae91aac6e
Dante Hoyle <hoyledante@gmail.com>
2024-02-25 19:05:55 -0800

    fixed broken aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,12 @@
 if status is-interactive
 	# aliases
 	if type --query nvim
-	    alias vim="nvim"
-	    alias vi = "nvim"
+	    alias vim nvim
+	    alias vi nvim
 	end
 
 	# config is an abbreviation for working with the bare git repository for dotfiles 
-	abbr config /usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME
+	abbr --add config /usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME
 
 	# environment variables
 	set -gx EDITOR /usr/bin/nvim
