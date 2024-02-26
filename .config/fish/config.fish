d2fa32de6603dba09d8e4f8b3167d0a17874db88
Dante Hoyle <hoyledante@gmail.com>
2024-02-25 21:47:38 -0800

    fixed vim alias in fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,13 @@
 if status is-interactive
-	# aliases
-	if type --query nvim
-	    alias vim nvim
-	    alias vi nvim
-	end
+    # aliases
+    if type --query nvim
+	alias vim /usr/bin/nvim
+	alias vi /usr/bin/vim
+    end
 
-	# config is an abbreviation for working with the bare git repository for dotfiles 
-	abbr --add config /usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME
+    # config is an abbreviation for working with the bare git repository for dotfiles 
+    abbr --add config /usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME
 
-	# environment variables
-	set -gx EDITOR /usr/bin/nvim
+    # environment variables
+    set -gx EDITOR /usr/bin/nvim
 end
