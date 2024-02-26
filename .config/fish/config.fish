b89daa51ae8b559f25a8c0381b6144f5ff818027
Dante Hoyle <hoyledante@gmail.com>
2024-02-26 09:41:17 -0800

    fixed whitespace issues on config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,10 @@
+# File: config.fish
+
 if status is-interactive
     # aliases
     if type --query nvim
-	alias vim /usr/bin/nvim
-	alias vi /usr/bin/vim
+        alias vim /usr/bin/nvim
+        alias vi /usr/bin/vim
     end
 
     # config is an abbreviation for working with the bare git repository for dotfiles 
