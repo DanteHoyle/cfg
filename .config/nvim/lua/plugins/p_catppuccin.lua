8b2c28db89b9d6e017eb1aed42a1f839c210baf2
Dante Hoyle <hoyledante@gmail.com>
2024-02-27 09:12:07 -0800

    fixed theme config settings

--- .config/nvim/lua/plugins/p_catppuccin.lua
+++ .config/nvim/lua/plugins/p_catppuccin.lua
@@ -3,9 +3,13 @@
 return { 
     "catppuccin/nvim", 
     name = "catppuccin", 
+    -- settings lazy and priority here makes sure this loads immediately
+    lazy = false, 
     priority = 1000,
+
     config = function()
+        -- loads the theme here
         require("catppuccin").setup()
-        vim.cmd.colorscheme "catppuccin"
+        vim.cmd([[colorscheme catppuccin-mocha]])
     end,
 }
