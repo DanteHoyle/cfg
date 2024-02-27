420175f9ac532986a6315dc44033f81a4fb7dacc
Dante Hoyle <hoyledante@gmail.com>
2024-02-26 21:50:14 -0800

    upadted theme to catppuccin-mocha

--- .config/nvim/lua/config/theme.lua
+++ .config/nvim/lua/config/theme.lua
@@ -4,4 +4,4 @@
 -- Sets up relative line numbering with absolute position on current line
 vim.opt.relativenumber = true
 vim.opt.nu = true
-vim.cmd.colorscheme = "catppuccin"
+vim.cmd.colorscheme = "catppuccin-mocha"
