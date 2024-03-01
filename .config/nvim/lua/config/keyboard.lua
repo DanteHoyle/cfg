d3d4c8d944fd09e0cda9490b02f73f6826915909
Dante Hoyle <hoyledante@gmail.com>
2024-03-01 01:18:20 -0800

    updated keybord shortcuts

--- .config/nvim/lua/config/keyboard.lua
+++ .config/nvim/lua/config/keyboard.lua
@@ -1,21 +1,33 @@
 -- File: hotkeys.lua
 
--- MOUSE ---------------------------------------
+--- Mouse ---
 -- Enable Mouse
 vim.opt.mouse = 'a'
 
---- Hot Keys -------------------------------------------------------------------
+--- General Remaps ---
 -- remaps <Leader>
 vim.g.mapleader = ";";
-
+-- maps F1 to clear highlighted matches
 vim.api.nvim_set_keymap("n", "<F1>", ":noh<CR>", { noremap = true, silent = true })
+
+--- Insert Mode Remaps ---
+-- Leader + b for bold in Neorg
+vim.api.nvim_set_keymap("i", "<Leader>b", "**<Esc>i", { noremap = true, silent = true })
+-- Leader + i for italic in Neorg 
+vim.api.nvim_set_keymap("i", "<Leader>i", "//<Esc>i", { noremap = true, silent = true })
+-- Leader + u for underline in Neorg
+vim.api.nvim_set_keymap("i", "<Leader>u", "__<Esc>i", { noremap = true, silent = true })
+
 --- My Scripts ---
 -- maps F12 to toggle between tabs and spaces
 vim.api.nvim_set_keymap("n", "<F12>", ":lua toggle_tabs()<CR>", { noremap = true, silent = true })
 
+
+--- Begin Plugin Config ---
 --- WhichKey ---
--- maps 2x Leader to open WhichKey
+-- maps Leader + Leader to open WhichKey
 vim.api.nvim_set_keymap("n", "<Leader><Leader>", ":WhichKey<CR>", { noremap = true, silent = true })
+
 --- NvimTree ---
 -- maps Leader +  to focus NvimTree
 vim.api.nvim_set_keymap("n", "<Leader><BS>", ":NvimTreeFocus<CR>", { noremap = true, silent = true })
@@ -25,3 +37,4 @@ vim.api.nvim_set_keymap("n", "<Leader><C-i>", ":NvimTreeToggle<CR>", { noremap =
 --- Telescope ---
 -- maps <Leader> + P to open telescope
 vim.api.nvim_set_keymap("n", "<C-p>", ":Telescope<CR>", { noremap = true, silent = true })
+--- End Plugin Config ---
