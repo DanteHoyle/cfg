9b89f94028db076eabefeda4101a904dea0d212d
Dante Hoyle <hoyledante@gmail.com>
2024-03-03 10:49:40 -0800

    updated abbreviations for config directories

--- .config/fish/conf.d/abbreviations.fish
+++ .config/fish/conf.d/abbreviations.fish
@@ -2,6 +2,9 @@
 # Description: adds abbreviations which auto-expand in a fish prompt
 
 if status is-interactive
-    # config is an abbreviation for working with the bare git repository for dotfiles 
-    abbr --add config cfg
+    # common directory abbreviations
+    abbr --add cnvim $NVIM_CONFIG_DIR
+    abbr --add ctmux $TMUX_CONFIG_DIR
+    abbr --add cfish $FISH_CONFIG_DIR
+    abbr --add ckitty $KITTY_CONFIG_DIR
 end
