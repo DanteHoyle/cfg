70045583eb836e5a42171cf41ed09a5d8f940d37
Dante Hoyle <hoyledante@gmail.com>
2024-03-03 18:46:50 -0800

    updated vim alias to check if nvim is installed

--- .config/nvim/lua/config/neorg-keybinds.lua
+++ .config/nvim/lua/config/neorg-keybinds.lua
@@ -1,3 +1,3 @@
 -- File: neorg-keybinds.lua
 -- maps <Leader> + n to open Neorg workspaces
-vim.api.nvim_set_keymap("n", "<Leader>n", ":Neorg workspaces<CR>", { noremap = true, silent = false })
+vim.api.nvim_set_keymap("n", "<Leader>n", ":Neorg workspaces", { noremap = true, silent = false })
