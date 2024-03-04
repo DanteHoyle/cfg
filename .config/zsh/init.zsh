0c0d10266659d93fee2877681aac8f65fac4e80c
Dante Hoyle <hoyledante@gmail.com>
2024-03-03 17:45:26 -0800

    moved .env to init.zsh

--- .config/zsh/init.zsh
+++ .config/zsh/init.zsh
@@ -1,4 +1,19 @@
-# File: init.zsh
+# File: init.zsh# Change default editor
+
+EDITOR=nvim
+
+# Frequneytly used folders
+VIM_CONFIG_DIR=~/.vim
+NVIM_CONFIG_DIR=~/.config/nvim
+FISH_CONFIG_DIR=~/.config/fish
+KITTY_CONFIG_DIR=~/.config/kitty
+TMUX_CONFIG_DIR=~/.config/tmux
+ZSH_CONFIG_DIR=~/.config/zsh
+
+# History settings
+HISTSIZE=10000
+SAVEHIST=10000
+HISTFILE=~/.cache/zsh/history
 
 for conf in ~/.config/zsh/conf.d/*; do
     source $conf
