63a06b29ebf77d99e8c1151bd93a9e26d2508972
Dante Hoyle <hoyledante@gmail.com>
2024-03-05 21:53:43 -0800

    Changed plugin loading in attempt to be distro agnostic

--- .config/zsh/init.zsh
+++ .config/zsh/init.zsh
@@ -20,7 +20,8 @@ for conf in ~/.config/zsh/conf.d/*; do
     source $conf
 done
 
-# load zsh-syntax-highlighting
-source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-# load zsh-autosuggestions for fish-like suggestions 
-source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+# attemps to load arch path, followed by debian / fedora path if that fails
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh || \
+    source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh || \
+    source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh 
