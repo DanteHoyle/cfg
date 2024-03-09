df9a17ff1044b9738197d1038d9263134d625ead
Dante Hoyle <hoyledante@gmail.com>
2024-03-08 22:35:37 -0800

    compressed zsh config into fewer files

--- .config/zsh/conf.d/vim.zsh
+++ .config/zsh/conf.d/vim.zsh
@@ -16,3 +16,13 @@ function zle-keymap-select {
 # Edit line in vim with ctrl-e:
 autoload edit-command-line; zle -N edit-command-line
 bindkey '^e' edit-command-line
+
+# enables emacs mode
+bindkey -e
+
+# use vim keys in tab complete menu
+# bindkey -M menuselect 'h' vi-backward-char
+# bindkey -M menuselect 'k' vi-up-line-or-history
+# bindkey -M menuselect 'l' vi-forward-char
+# bindkey -M menuselect 'j' vi-down-line-or-history
+# bindkey -v '^?' backward-delete-char
--- .config/zsh/init.zsh
+++ .config/zsh/init.zsh
@@ -4,24 +4,26 @@
 command -v nvim >/dev/null && EDITOR=nvim || EDITOR=vim
 
 # Frequneytly used folders
-VIM_CONFIG_DIR=~/.vim
-NVIM_CONFIG_DIR=~/.config/nvim
-FISH_CONFIG_DIR=~/.config/fish
-KITTY_CONFIG_DIR=~/.config/kitty
-TMUX_CONFIG_DIR=~/.config/tmux
-ZSH_CONFIG_DIR=~/.config/zsh
+VIM_CONFIG_DIR="$HOME/.vim"
+NVIM_CONFIG_DIR="$HOME~/.config/nvim"
+FISH_CONFIG_DIR="$HOME/.config/fish"
+KITTY_CONFIG_DIR="$HOME/.config/kitty"
+TMUX_CONFIG_DIR="$HOME/.config/tmux"
+ZSH_CONFIG_DIR="$HOME/.config/zsh"
 
 # History settings
 HISTSIZE=10000
 SAVEHIST=10000
-HISTFILE=~/.cache/zsh/history
+HISTFILE="~/.cache/zsh/history"
 
-for conf in ~/.config/zsh/conf.d/*; do
-    source $conf
-done
+source "$ZSH_CONFIG_DIR/conf.d/aliases.zsh"
+source "$ZSH_CONFIG_DIR/conf.d/prompt.zsh"
 
 # attemps to load arch path, followed by debian / fedora path if that fails
-source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh || \
-    source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh || \
-    source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh 
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh 2> /dev/null || \
+    source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh 2> /dev/null
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh 2> /dev/null || \
+    source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh 2> /dev/null
+
+source "$ZSH_CONFIG_DIR/conf.d/vim.zsh"
+
