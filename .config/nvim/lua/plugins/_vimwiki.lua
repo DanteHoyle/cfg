eb3e35f86d119919525a225b33095b0c3eb40a41
Dante Hoyle <hoyledante@gmail.com>
2024-04-17 20:02:31 -0700

    converted vimwiki syntax to use markdown

--- .config/nvim/lua/plugins/_vimwiki.lua
+++ .config/nvim/lua/plugins/_vimwiki.lua
@@ -1,3 +1,16 @@
+-- File: _vimwiki.lua
+-- Description: Plugin for organizing markdown notes
+
 return {
-    "vimwiki/vimwiki"
+    "vimwiki/vimwiki",
+
+    init = function()
+        vim.g.vimwiki_list = {
+            {
+                path = "~/documents/notes/",
+                syntax = "markdown",
+                ext = ".md"
+            }
+        }
+    end
 }
