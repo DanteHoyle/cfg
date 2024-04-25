370e3b9ba54ad732edb26dff08ce58924ff97fd9
Dante Hoyle <hoyledante@gmail.com>
2024-04-24 19:53:25 -0700

    added catppuccin-macchiato flavor back to neovim config

---
+++ .config/nvim/lua/plugins/_catppuccin.lua
@@ -0,0 +1,12 @@
+-- File: catppuccin.lua
+-- Description: Load and enable catppuccin plugin from https://github.com/catppuccin/nvim?tab=readme-ov-file
+
+return {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        vim.cmd.colorscheme "catppuccin-macchiato"
+    end,
+}
