c97ca16fcc127ec7206f1fbb2713d7decd39005b
Dante Hoyle <hoyledante@gmail.com>
2024-04-28 15:00:12 -0700

    updates notes script to use fzf and be more user friendly overall

--- .local/scripts/notes.sh
+++ .local/scripts/notes.sh
@@ -2,7 +2,10 @@
 # File: notes.sh
 # Description: Wrapper script for opening notes directory
 
-NOTES_DIRECTORY="~/documents/notes
+DEFAULT_NOTES_DIRECTORY="$HOME/documents/notes"
 
-cd $NOTES_DIRECTORY && \
-    nvim $(fzf)
+# find lists all fiels in the notes directory
+# grep removes listings in the .meta folder since neovim doesn't need to open images
+# fzf gives me a nice user friendly selection menu. The selected file is sent to neovim.
+cd $DEFAULT_NOTES_DIRECTORY && \
+    nvim $( find | cut --characters="3-" | grep -v ".meta" | fzf )
