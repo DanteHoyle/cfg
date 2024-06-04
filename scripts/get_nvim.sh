e6997acb1c0b8ed5e826f81f2d9653ff804828c2
Dante Hoyle <hoyledante@gmail.com>
2024-06-03 20:23:27 -0700

    Updated installer script for nvim

--- scripts/get_nvim.sh
+++ scripts/get_nvim.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+echo "Installing NVIM"
+
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
