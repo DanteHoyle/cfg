63677526ee19199ef2392925b8e60134124eb444
Dante Hoyle <hoyledante@gmail.com>
2024-06-03 21:23:29 -0700

    added comments and finished get_nvim.sh

--- scripts/get_nvim.sh
+++ scripts/get_nvim.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
-echo "Installing NVIM"
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+# remove previous install if it exists
 sudo rm -rf /opt/nvim
+
+# remove previous install symbolic link if one exists
+rm /usr/local/bin/nvim
+
+# downloads and extracts nvim to /opt/nvim-linux64
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
-# ln -s /opt/nvim-linux64/bin/nvim 
+# create symbolic link to a location path already includes
+sudo ln -s /opt/nvim-linux64/bin/nvim /usr/local/bin/nvim
+
+# clean install archive
+rm nvim-linux64.tar.gz
