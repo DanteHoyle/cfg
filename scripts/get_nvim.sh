8c1aff7534ddc8273fad1394836ca198f1aa954f
Dante Hoyle <hoyledante@gmail.com>
2024-06-03 20:00:29 -0700

    removed unused scripts and added get_nvim.sh

---
+++ scripts/get_nvim.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+# ln -s /opt/nvim-linux64/bin/nvim 
