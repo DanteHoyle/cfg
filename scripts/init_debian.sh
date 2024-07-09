234077c1d50ffb5d7f556a8029693e7274e729bd
Dante Hoyle <hoyledante@gmail.com>
2024-07-09 15:36:27 -0700

    started adding different system types to init-debian.sh starting with base, dev, and fun

--- scripts/init_debian.sh
+++ scripts/init_debian.sh
@@ -2,12 +2,32 @@
 # File: init_debian.sh
 # Description: Downloads useful files for new debian installs
 
-sudo apt-get install zsh \
-                     man \
-                     git \
-                     curl \
-                     gcc \
-                     tmux \
-                     keychain \
-                     shellcheck \
-                     mtr-tiny
+base="yes"
+dev="yes"
+fun="no"
+
+if [ -n "$base" ]; then
+    echo SCRIPT: Installing base packages
+    sudo apt-get install zsh \
+                         man \
+                         git \
+                         curl \
+                         ripgrep \
+                         mtr-tiny
+fi
+
+# dev tools
+if [ -n "$dev" ]; then
+    echo SCRIPT: Installing dev packages
+    sudo apt-get install shellcheck \
+                         tmux \
+                         gcc \
+                         keychain \
+                         npm
+fi
+
+# fun programs
+if [ -n "$fun" ]; then
+    echo SCRIPT: Installing fun packages
+    sudo apt-get install cowsay
+fi
