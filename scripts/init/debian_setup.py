5c1eb8939a5e77f9db3072da2c777014dc8de3fb
Dante Hoyle <hoyledante@gmail.com>
2024-07-29 11:30:18 -0700

    updated python version of debian installer

--- scripts/init/debian_setup.py
+++ scripts/init/debian_setup.py
@@ -7,14 +7,21 @@ import subprocess
 import shlex
 import argparse
 
-def parse_args():
+def parse_args() -> tuple[str, bool]:
     """Handles arguments passed to script when ran in prompt"""
     parser = argparse.ArgumentParser(prog='debian_setup.py',
                                      description='This script conveniently downloads debian packages listed in a file',
                                      epilog='Created July 2024')
-    parser.add_argument()
 
-def get_package_list(text_file: str = 'debian_packages') -> list[str]:
+    parser.add_argument('-s', '--safe', help='echos apt-get command instead of running it', 
+                                        action='store_true', 
+                                        default=False)
+    parser.add_argument('-f', '--file_path', help='path of debian_packages', default='debian_packages')
+
+    args = parser.parse_args()
+    return args.packages, args.safe
+
+def get_package_list_from_file(text_file: str = 'debian_packages') -> list[str]:
     """A wrapper function for shlex.split"""
 
     with open(text_file, 'r', encoding='UTF-8') as file:
@@ -23,30 +30,32 @@ def get_package_list(text_file: str = 'debian_packages') -> list[str]:
 
 
 def download_packages_from_list(package_list: list[str],
-                                safe: bool = True) -> subprocess.CompletedProcess[bytes] | None:
+                                safe: bool) -> int | None:
     """ Uses apt-get to install packages from a user provided list """
 
     command_statement_list = ['sudo', 'apt-get', 'install'] + package_list
     # for c in command_statement_list:
     #     print(c)
+    user_input: str | None = None
 
     if safe:
-        user_input = input("Do you you want to run this command? (y/n): ").strip().lower()
+        print(f'auto-generated command: {" ".join(command_statement_list)}')
+        user_input = input('Do you you want to run this command? (y/n): ').strip().lower()
 
-    if safe is False or user_input in ('y', 'yes'):
-        return subprocess.run(command_statement_list, check=True, capture_output=True)
+    if user_input in ('y', 'yes', None):
+        return subprocess.run(command_statement_list, check=True).returncode
     elif user_input in ('n', 'no'):
         return None
     else:
-        raise ValueError("Incorrect input for (y/n)")
+        raise ValueError(f'User input is not out in expected range. {user_input} is not in ("yes", "y", "no", "n")')
 
 
 def main():
     """ debian_setup.py main function """
-    package_list = get_package_list()
-    output = download_packages_from_list(package_list)
+    file, safe = parse_args()
+    debian_packages = get_package_list_from_file(text_file=file)
+    download_packages_from_list(debian_packages, safe=safe)
 
-    print(output)
 
 
 if __name__ == '__main__':
