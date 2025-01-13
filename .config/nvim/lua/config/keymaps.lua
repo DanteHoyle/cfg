3642434bdd305e629c7d2de427b391323b601d24
Dante Hoyle <hoyledante@gmail.com>
2025-01-13 08:40:45 -0800

    updated neovim keymaps and updated plugins

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,7 +6,7 @@
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear highlights' })
 
 -- Toggle line wrapping with Leader + w
-vim.keymap.set('n', '<Leader>w', '<cmd>set wrap!<CR>', { desc = 'Toggle line wrapping' })
+vim.keymap.set('n', '<Leader>W', '<cmd>set wrap!<CR>', { desc = 'Toggle line wrapping' })
 
 -- Center the screen when moving up or down in a file
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
@@ -15,14 +15,6 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz')
 -- INSERT MODE
 -- Delete words in insert mode with Alt + Backspace
 vim.keymap.set('i', '<M-BS>', '<C-W>')
-
--- VISUAL MODE
--- Move lines
-vim.keymap.set('v', 'J', ':m \'>+1<CR>gv=gv', { desc = 'Move block one line down' })
-vim.keymap.set('v', 'K', ':m \'<-2<CR>gv=gv', { desc = 'Move block one line up' })
-
--- TERMINAL MODE
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- LSP
 vim.keymap.set('n', '<Leader>r', function() vim.lsp.buf.rename() end, { desc = 'LSP Rename' } )
