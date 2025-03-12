cdf51b606e21217df8bd700e71237dbca5027a00
Dante Hoyle <hoyledante@gmail.com>
2025-03-11 18:59:54 -0700

    Added LSP back to neovim

---
+++ .config/nvim/lua/plugins/language.lua
@@ -0,0 +1,75 @@
+return {
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            { 'williamboman/mason.nvim', config=true },
+            'williamboman/mason-lspconfig.nvim'
+        },
+        config = function()
+            require('mason').setup()
+            require('mason-lspconfig').setup {
+                ensure_installed = { 'lua_ls', 'pyright' },
+            }
+
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+
+            require('lspconfig').pyright.setup {
+                capabilities = capabilities,
+                settings = {
+                    python = {
+                        analysis = {
+                            typeCheckingMode = 'off',
+                        }
+                    },
+                }
+            }
+
+            require('lspconfig').ts_ls.setup {
+                capabilities = capabilities
+
+            }
+
+            -- Small helper function to map keys to telescope functions
+            local map = function(keys, func, desc, mode)
+                mode = mode or 'n'
+                vim.keymap.set(mode, keys, func, { desc = 'LSP: ' .. desc })
+            end
+
+            local builtin = require('telescope.builtin')
+
+            map('gd', builtin.lsp_definitions, '[G]oto [D]efinition')
+            map('gr', builtin.lsp_references, '[G]oto [R]eferences')
+            map('gI', builtin.lsp_implementations, '[G]oto [I]mplementation')
+            map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+            map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+            vim.diagnostic.config {
+                severity_sort = true,
+                float = { border = 'rounded', source = 'if_many' },
+                underline = { severity = vim.diagnostic.severity.ERROR },
+                signs = vim.g.have_nerd_font and {
+                    text = {
+                        [vim.diagnostic.severity.ERROR] = '󰅚 ',
+                        [vim.diagnostic.severity.WARN] = '󰀪 ',
+                        [vim.diagnostic.severity.INFO] = '󰋽 ',
+                        [vim.diagnostic.severity.HINT] = '󰌶 ',
+                    },
+                } or {},
+                virtual_text = {
+                    source = 'if_many',
+                    spacing = 2,
+                    format = function(diagnostic)
+                        local diagnostic_message = {
+                            [vim.diagnostic.severity.ERROR] = diagnostic.message,
+                            [vim.diagnostic.severity.WARN] = diagnostic.message,
+                            [vim.diagnostic.severity.INFO] = diagnostic.message,
+                            [vim.diagnostic.severity.HINT] = diagnostic.message,
+                        }
+                        return diagnostic_message[diagnostic.severity]
+                    end,
+                },
+            }
+        end,
+    }
+}
