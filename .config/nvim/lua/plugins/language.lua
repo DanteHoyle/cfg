19a41d94f5da11519677f3f06a44634944b4530a
Dante Hoyle <hoyledante@gmail.com>
2025-04-05 12:06:17 -0700

    Updated neovim plugins and fixed treesitter

--- .config/nvim/lua/plugins/language.lua
+++ .config/nvim/lua/plugins/language.lua
@@ -27,11 +27,13 @@ return {
                     documentation = cmp.config.window.bordered(),
                 },
                 mapping = cmp.mapping.preset.insert({
+                    ['<C-n>'] = cmp.mapping.select_next_item(),
+                    ['<C-p>'] = cmp.mapping.select_prev_item(),
                     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.abort(),
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    ['<C-y>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
                 }),
                 sources = cmp.config.sources({
                     { name = 'nvim_lsp' },
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,14 +3,12 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    main = 'nvim-treesitter.configs',
     dependencies = 'hiphish/rainbow-delimiters.nvim',
     opts = {
         auto_install = true,
         ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'python'},
-        highlight = {
-            enable = true,
-        },
+        highlight = { enable = true, },
         indent = { enable = true },
     }
-
 }
