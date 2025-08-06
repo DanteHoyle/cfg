4d30fe3bfe254e95ed6aa5fc2e695363b837ee74
Dante Hoyle <hoyledante@gmail.com>
2025-08-06 14:35:19 -0700

    updated tmux config and lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -11,13 +11,13 @@ return {
     sections = {
       lualine_b = {'branch', 'diff'}
     },
-    tabline = {
-      lualine_a = {'buffers'},
-      lualine_b = {},
-      lualine_c = {},
-      lualine_x = {'diagnostics', 'lsp_status'},
-      lualine_y = {'searchcount'},
-      lualine_z = {}
-    }
+    -- tabline = {
+    --   lualine_a = {'buffers'},
+    --   lualine_b = {},
+    --   lualine_c = {},
+    --   lualine_x = {'diagnostics', 'lsp_status'},
+    --   lualine_y = {'searchcount'},
+    --   lualine_z = {}
+    -- }
   }
 }
