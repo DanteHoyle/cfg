91c01db64fdcbe4a377f63b0e3f4130ba9f4b5b6
Dante Hoyle <hoyledante@gmail.com>
2025-08-25 17:04:20 -0700

    updated neovim markdown settings

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -8,16 +8,6 @@ return {
       section_separators = '',
       component_separators = '',
     },
-    sections = {
-      lualine_b = {'branch', 'diff'}
-    },
-    -- tabline = {
-    --   lualine_a = {'buffers'},
-    --   lualine_b = {},
-    --   lualine_c = {},
-    --   lualine_x = {'diagnostics', 'lsp_status'},
-    --   lualine_y = {'searchcount'},
-    --   lualine_z = {}
-    -- }
+    sections = { lualine_b = {'branch', 'diff'} },
   }
 }
